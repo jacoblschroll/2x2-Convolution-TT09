e173ac38d7d3e03d5dafe10c079b374420319545
Jacob Schroll <jacoblschroll@gmail.com>
2024-10-16 05:53:15 -0500

    Added previous work

--- src/project.v
+++ src/project.v
@@ -16,12 +16,53 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  // 2x2, each value is 8-bits
+  reg [31:0] inputs;
+  reg [31:0] weights;
+  
+  reg [17:0] greatest;
+  
+  reg [17:0] convolution;
+  
+  reg [9:0] outputState;
+  
+  // Stores if the clock counts are odd or even
+  reg odd = 1'b0;
+  
+  assign uio_oe[1:0] = 1;
+  assign uio_oe[7:2] = 0;
+
+  assign uio_in[5:0] = 0;
+  assign uio_out[7:2] = 0;
+
+  always @ (posedge clk) begin
+    if (!rst_n) begin
+      inputs <= 32'b0;
+      weights <= 32'b0;
+      odd <= 1'b0;
+    end else if (uio_in[7]) begin
+      outputState <= {odd, greatest[8:0]};
+      odd <= !odd;
+    end else if (uio_in[6]) begin
+      weights <= {ui_in[7:0], weights[31:8]};
+    end else begin
+      inputs <= {ui_in[7:0], inputs[31:8]};
+    end
+    
+    convolution <= (inputs[7:0] * weights[7:0]);
+    convolution <= convolution + (inputs[15:8] * weights[15:8]);
+    convolution <= convolution + (inputs[23:16] * weights[23:16]);
+    convolution <= convolution + (inputs[31:24] * weights[31:24]);
+    
+    if (convolution > greatest) begin
+      greatest <= convolution;
+    end
+  end
+
+  assign uo_out = outputState[7:0];
+  assign uio_out[1:0] = outputState[9:8];
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
 
 endmodule
